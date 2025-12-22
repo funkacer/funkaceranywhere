12fe03a1a15b210389261e4cd884b4faa0dbf941
Jiří Černý <funkacer@gmail.com>
2025-12-22 10:30:10 +0100

    First commit - funkacer layout

---
+++ app.py
@@ -0,0 +1,37 @@
+# mkdir froshims_flask_app
+# cd froshims_flask_app
+# cp ../my_flask_app/requirements.txt .
+# . ../../bin/activate (. _Git/_test/bin/activate)
+# pip install -r requirements.txt
+# touch app.py
+# mkdir templates
+# touch layout.html index.html
+# flask run
+
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
+import sqlite3
+
+app = Flask(__name__)
+
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
+# pro pythonanywhere:
+# DB = "mysite/store.db"
+# pro localhost:
+# DB = "store.db"
+
+@app.route("/")
+def index():
+    #con = sqlite3.connect(DB)
+    #con.row_factory = sqlite3.Row
+    #cur = con.execute("SELECT * FROM books")
+    #books = cur.fetchall()
+    #con.close()
+    #return render_template("books.html", books=books)
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
